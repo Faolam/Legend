367cd1b8547618bf5ec4e6c16939922c8b95b3bd
Pedro Henrique <groupraccoons@gmail.com>
2021-09-22 20:29:42 -0300

    Id de Emojis

--- src/events/message-event.js
+++ src/events/message-event.js
@@ -1,6 +1,7 @@
 // Importações
 const { Bot_Info, Adm_Priv } = require("../config/config.json");
 const { Claras, Escuras } = require("../config/colors.json")
+const { Estaticos, Animados } = require("../config/emojis.json")
 
 // Execução do comando.
 module.exports = {
@@ -15,7 +16,7 @@ module.exports = {
             if (cmd.help.aliases.includes(command) === true) return cmd
         })
 
-        if (command === undefined) return msg.channel.send(":pepeJamSides: Salve " + msg.author.toString() + " !\nNão fui capaz de reconhecer o comando `" + msg.content.slice(Bot_Info.Defaut_Prefix.length) + "`, tente-o de **outra** forma!") 
+        if (command === undefined) return msg.channel.send(Animados.pepeJamSides + " Salve " + msg.author.toString() + " !\nNão fui capaz de reconhecer o comando `" + msg.content.slice(Bot_Info.Defaut_Prefix.length) + "`, tente-o de **outra** forma!") 
 
         const perms = command.help.status
 
